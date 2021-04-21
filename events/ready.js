3aa57c887b89b3fc537c3a49fc927c14d0029c60
cortexisgod <82268009+cortexisgod@users.noreply.github.com>
2021-04-22 00:56:06 +0300

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,15 @@
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+  console.log(`[BOT] | Botun destekçileri ve komutları başarıyla yüklendi, işlemler tamamlandı !`);
+  console.log(`[BOT] | (${client.user.username}) Cortex:) !
+  ‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒`);
+  console.log(`HESK`);
+  client.user.setPresence({ activity: { name: "HESK" }, status: "idle" });
+
+};
